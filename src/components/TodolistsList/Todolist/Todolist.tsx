a8c9dd7a4aff7108f607b39e8536230f8e39b759
KauhareniaNastassia <kavgorenya_a@tut.by>
2023-03-31 23:37:48 +0300

    add some styles

--- src/components/TodolistsList/Todolist/Todolist.tsx
+++ src/components/TodolistsList/Todolist/Todolist.tsx
@@ -4,6 +4,8 @@ import {TaskResponseType} from "../../../api/task-api";
 import {Task} from "./Task/Task";
 import {useAppDispatch, useAppSelector} from "../../../hooks/hooks";
 import {gatTasksTC} from "../../../store/task-reducer";
+import {IconButton} from "@mui/material";
+import {Delete} from "@material-ui/icons";
 
 
 type TodolistPropsType = {
@@ -24,20 +26,32 @@ export const Todolist:React.FC<TodolistPropsType> = ({
     }, [dispatch, todolist.id])
 
     return (
-        <div key={todolist.id}>
-            {
-                tasks?.map(task => {
-                    return (
-                        <Task
-                            key={task.id}
-                            todolistId={todolist.id}
-                            task={task}
+        <div>
+            <h3>
+                {todolist.title}
+            </h3>
+            <IconButton>
+                <Delete/>
+            </IconButton>
 
-                        />
-                    )
-                })
-            }
+
+            <div key={todolist.id}>
+                {
+                    tasks?.map(task => {
+                        return (
+                            <Task
+                                key={task.id}
+                                todolistId={todolist.id}
+                                task={task}
+
+                            />
+                        )
+                    })
+                }
+            </div>
         </div>
+
+
     );
 };
 
--- src/components/TodolistsList/TodolistsList.tsx
+++ src/components/TodolistsList/TodolistsList.tsx
@@ -1,23 +1,18 @@
 import React, {useEffect} from 'react';
-import {Grid} from "@mui/material";
+import {Grid, Paper} from "@mui/material";
 import {getTodolistsTC} from "../../store/todolist-reducer";
 import {useAppDispatch, useAppSelector} from "../../hooks/hooks";
 import {Todolist} from "./Todolist/Todolist";
 
 
-
-
-
-
-
-export const TodolistsList:React.FC = () => {
+export const TodolistsList: React.FC = () => {
     const dispatch = useAppDispatch()
     const todolists = useAppSelector(state => state.todolists)
     const tasks = useAppSelector(state => state.tasks)
     const isLoggedIn = useAppSelector(state => state.login.isLoggedIn)
 
     useEffect(() => {
-        if(!isLoggedIn) {
+        if (!isLoggedIn) {
             return
         }
         dispatch(getTodolistsTC())
@@ -25,14 +20,22 @@ export const TodolistsList:React.FC = () => {
 
     return (
         <div>
+            <Grid container style={{padding: '20px'}}>
+                AddItem
+            </Grid>
+
             <Grid container spacing={3}>
                 {
                     todolists.map(tl => {
-                        return <Todolist
-                            key={tl.id}
-                            todolist={tl}
-                            tasks={tasks[tl.id]}
-                        />
+                        return <Grid item key={tl.id}>
+                            <Paper style={{padding: '10px'}}>
+                                <Todolist
+                                    key={tl.id}
+                                    todolist={tl}
+                                    tasks={tasks[tl.id]}
+                                />
+                            </Paper>
+                        </Grid>
                     })
                 }
 
